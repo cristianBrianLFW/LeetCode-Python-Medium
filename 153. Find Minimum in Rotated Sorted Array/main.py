187892db452b632a6f096d0ded44298d7312d4c5
Lloyd <lloydfronteraeng@gmail.com>
2026-05-21 19:57:56 -0300

    consetando o erro 153

--- 153. Find Minimum in Rotated Sorted Array/main.py
+++ 153. Find Minimum in Rotated Sorted Array/main.py
@@ -3,22 +3,17 @@ from typing import List
 class Solution:
     def findMin(self, nums: List[int]) -> int:
 
-        l, r = 0, len ( nums ) - 1
+        def b_search ( l : int, r : int ) -> int:
 
-        def b_search ( l: int, r: int ) -> int:
+            if l < r:
+                m = ( l + r) // 2
 
-            while l <= r:
-
-                m = ( l + r ) // 2
-
-                if nums [ m ] < nums [ r ]:
-
+                if nums [ m ] > nums [ r ]:
                     return b_search ( m + 1, r)
-                else:
-                    return b_search ( l, m - 1)
+                else :
+                    return b_search  ( l, m )
             return l
-        
-        return b_search ( l, r)
+        return nums [ b_search ( 0, len ( nums ) - 1)]
 
 nums = [3,4,5,1,2]
 
